5d7019876e56e1525e6320d34f9b9a7d8a6fdd18
ZvegintsevaAlisa <102322490+ZvegintsevaAlisa@users.noreply.github.com>
2022-06-02 15:45:56 +0300

    Add files via upload

---
+++ hamilton.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <locale.h>
+#include <vector>
+
+using namespace std;
+vector <bool> vis;
+vector <int> path;
+bool hmltn(int curr) {
+    int const n = 6;
+    int M[n][n] = { {0, 1, 0, 1, 1, 1},
+                       {1, 0, 1, 1, 1, 0},
+                       {0, 1, 0, 1, 1, 0},
+                       {1, 1, 1, 0, 0, 1},
+                       {1, 1, 1, 0, 0, 0},
+                       {1, 0, 0, 1, 0, 0} };
+    path.push_back(curr);//добавляем вершину  в вектор
+    if (path.size() == n) //проверяем, не получен ли полный цикл(если в вектор вошли все вершины и между 1 и последней есть ребро)
+        if (M[path[0]][path.back()] == 1)
+            return true;
+        else {
+            path.pop_back();
+            return false;
+        }
+    vis[curr] = true;
+    for (int nxt = 0;nxt < n; nxt++)
+        if (M[curr][nxt] == 1 && !vis[nxt]) //проверяем, есть ли цикл через соседние непосещенные вершины
+            if (hmltn(nxt)) return true;
+    vis[curr] = false; //если цикла нет, то отмечаем вершину непосещенной и удаляем из вектора 
+    path.pop_back();
+    return false;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int n = 6;
+    int MatSm[n][n] = { {0, 1, 0, 1, 1, 1},
+                       {1, 0, 1, 1, 1, 0},
+                       {0, 1, 0, 1, 1, 0},
+                       {1, 1, 1, 0, 0, 1},
+                       {1, 1, 1, 0, 0, 0},
+                       {1, 0, 0, 1, 0, 0} };
+    for (int i = 0; i < n; i++)
+        vis.push_back(false);
+    for (int i = 0; i < n; i++)
+        hmltn(i);
+    cout << "Гамильтонов цикл:";
+    for (int i = 0; i < path.size(); i++)
+        cout << path[i] << " -> ";
+    cout << path[0];
+    return 0;
+}
---
+++ kr_puti_floid.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <locale.h>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int n = 6;
+    int MatSm[n][n] = { {0, 7, 9, 0, 0, 14},
+                       {7, 0, 10, 15, 0, 0},
+                       {9, 10, 0, 11, 0, 2},
+                       {0, 15, 11, 0, 6, 0},
+                       {0, 0, 0, 6, 0, 9},
+                       {14, 0, 2, 0, 9, 0} };
+    int Fl[n][n] = { 0 };
+    int path[n][n];
+    int i, j, k;
+    for (i = 0; i < n; i++)
+        for (j = 0;j < n;j++) //матрица путей
+        {
+            path[i][j] = j + 1;
+        }
+    for (i = 0; i < n; i++)
+        for (j = 0;j < n;j++)
+            if (MatSm[i][j] == 0 && i != j)
+                Fl[i][j] = 999;
+            else
+                Fl[i][j] = MatSm[i][j];
+    for (i = 0; i < n; i++) //проверяем, не короче ли пути напрямую путь из j в k через i 
+        for (j = 0;j < n;j++)
+            for (k = 0;k < n;k++)
+                if (Fl[j][i] + Fl[i][k] < Fl[j][k]) //если да, то меняем вес пути в матрице и в матрице путей запоминаем, через какую вершину i нашли путь  
+                {
+                    Fl[j][k] = Fl[j][i] + Fl[i][k];
+                    path[j][k] = i + 1;
+                }
+    cout << "Матрица длин кратчайших путей:" << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0;j < n;j++)
+            cout << Fl[i][j] << " ";
+        cout << endl;
+    }
+    cout << "Матрица путей:" << endl; 
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0;j < n;j++)
+            cout << path[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
